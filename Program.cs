3dff79a66e9bc2bb0da70f9b2693a13e0f16ee22
Kerem Durak <54421885+kerem97@users.noreply.github.com>
2021-05-10 04:28:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("     Languages     ");
+            Console.WriteLine("  1-   Turkish     ");
+            Console.WriteLine("  2-   English     ");
+            Console.WriteLine("  3-   Deutsch     ");
+            Console.WriteLine("  4-   French     ");
+            Console.Write("     Please choose  :   ");
+            int sec = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            switch (sec)
+            {
+                case 1: Console.WriteLine("     Seni Seviyorum     ");
+                    break;
+                case 2:
+                    Console.WriteLine("     I Love You     ");
+                    break;
+                case 3:
+                    Console.WriteLine("     Ich Liebe Dich     ");
+                    break;
+                case 4:
+                    Console.WriteLine("    Je t'aime     ");
+                    break;
+                default:Console.WriteLine("Wrong choice");
+                    break;
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
